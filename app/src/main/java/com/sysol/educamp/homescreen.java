985b3b595477da4e560cfdf995d56ec531f4be3e
Benjith <mpbenjith@gmail.com>
2018-01-31 13:56:23 +0530

    login page check network
    loginpage checks network is connected or not later redirect automaticaly into MainDash

--- app/src/main/java/com/sysol/educamp/homescreen.java
+++ app/src/main/java/com/sysol/educamp/homescreen.java
@@ -2,8 +2,13 @@ package com.sysol.educamp;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class homescreen extends AppCompatActivity {
 
@@ -11,13 +16,44 @@ public class homescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
+
             public void run() {
-                Intent intent=new Intent(homescreen.this,login.class);
-                startActivity(intent);
-                finish();
+
+                checkConnection();
+
             }
         },2000);
+
     }
+ protected boolean  connection(){
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+
+            return  true;
+        }else{
+
+             return false;
+        }
+
+
+    }
+    public void checkConnection(){
+        if (connection()){
+            Intent intent=new Intent(homescreen.this,login.class);
+            startActivity(intent);
+            finish();
+        }else {
+            Toast.makeText(homescreen.this, "Check internet connection", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
